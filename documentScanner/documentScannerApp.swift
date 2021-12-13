0626a0eb733b6ffe7c518d7a0ab2794420c0e74a
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-13 16:21:55 -0500

    Initial Commit

---
+++ documentScanner/documentScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  documentScannerApp.swift
+//  documentScanner
+//
+//  Created by Vincent Cloutier on 2021-12-13.
+//
+
+import SwiftUI
+
+@main
+struct documentScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
